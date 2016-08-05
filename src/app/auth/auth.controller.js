db4d8945c7a631b30e1173fbbe350a209915098e
gnamestnik <gnamestnik@keyengineering.com.au>
2016-08-05 14:46:30 +0800

    Set up Satellizer for registration

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -1,4 +1,14 @@
 export class AuthController {
 
+    constructor($auth){
+        'ngInject';
+
+        this.$auth = $auth
+    }
+
+    register() {
+        //console.log("test")
+        this.$auth.signup({email: 'test@test.com'});
+    }
 
 }
